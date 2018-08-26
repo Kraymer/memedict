c855983b8c47b1a89c808ade98763b950d3cf56e
Fabrice Laporte <fabrice@yescapa.com>
2018-08-26 23:04:12 +0200

    add similarity threshold

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # memedict
+
 Knowyourmeme.com definitions scraper
+
+## Install
+
+    pip install memedict
+    
+## Usage
+
+    >>> from memedict import search
+    >>> search('base belong to us')
+    u'"All Your Base Are Belong to Us" is a popular engrish catchphrase that grew popular across the internet as early as in 1998. An awkward translation of "all of your bases are now under our control", the quote originally appeared in the opening dialogue of Zero Wing, a 16-bit shoot\'em up game released in 1989. Marked by poor grammar, the "All Your Base" phrase and the dialogue scene went viral on popular discussion forums in 2000, spawning thousands of image macros and flash animations featuring the slogan both on the web and in real life.'
+
--- memedict/__init__.py
+++ memedict/__init__.py
@@ -6,10 +6,12 @@
 
 import requests
 from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
 
 __author__ = 'Fabrice Laporte <kraymer@gmail.com>'
 __version__ = '0.1.0-dev'
 
+SEARCH_SIMILARITY_THRESHOLD = .5
 
 HEADERS = {'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 '
         '(KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')}
@@ -18,16 +20,22 @@ HEADERS = {'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) Apple
 def search_meme(text):
     """Return a meme url from a meme keywords.
     """
-    r = requests.get('http://knowyourmeme.com/search?q=%s' % 'base belong to us', headers=HEADERS)
+    r = requests.get('http://knowyourmeme.com/search?q=%s' % text, headers=HEADERS)
     soup = BeautifulSoup(r.text, 'html.parser')
     memes_list = soup.find(class_='entry_list')
-    meme_path = memes_list.find('a', href=True)['href']
-    return 'https://knowyourmeme.com%s' % meme_path
+    if memes_list:
+        meme_path = memes_list.find('a', href=True)['href']
+        return meme_path.replace('-', ' '), 'https://knowyourmeme.com%s' % meme_path
+    return None, None
 
 
 def search(text):
     """Return a meme definition from a meme keywords.
     """
-    r = requests.get(search_meme(text), headers=HEADERS)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    return(soup.find('h2').next.next.next.text)
+    text = ' '.join([x for x in text.split() if len(x) > 3])
+    meme_name, url = search_meme(text)
+    if meme_name and SequenceMatcher(None, text, meme_name).ratio() > SEARCH_SIMILARITY_THRESHOLD:
+        r = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        entry = soup.find('h2', {'id': 'about'})
+        return(entry.next.next.next.text)
